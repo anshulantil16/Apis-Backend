204fad49427ce504d86b02d9f5972a6fdcef035f
anshulantil16 <anshul@apisindia.com>
2026-07-08 09:32:55 +0530

    Fix: remove undefined PMSLoginView from imports

--- pms/urls.py
+++ pms/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import PMSListView, PMSImportView, PMSEmployeeUpdateView, PMSTemplateView, PMSExportView, PMSLoginView
+from .views import PMSListView, PMSImportView, PMSEmployeeUpdateView, PMSTemplateView, PMSExportView
 
 urlpatterns = [
-    path('login/',                  PMSLoginView.as_view()),
     path('employees/',              PMSListView.as_view()),
     path('import/',                 PMSImportView.as_view()),
     path('employees/<int:emp_id>/', PMSEmployeeUpdateView.as_view()),
